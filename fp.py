896a2c9b662a37965e52aefb5af0663a3fe9c748
khandaresayali <sayalikhandare123@gmail.com>
2024-03-01 00:49:52 +0530

    Background Changed

--- fp.py
+++ fp.py
@@ -18,37 +18,37 @@ class MainApp(tk.Tk):
         self.create_widgets()
 
     def create_widgets(self):
-        self.bg_image = Image.open("assets/pix.jpg")
+        self.bg_image = Image.open("assets/Pixelize .png")
         self.bg_resize=self.bg_image.resize((1000,800))
         self.bg_image = ImageTk.PhotoImage(self.bg_image)
         self.bg_label = tk.Label(self, image=self.bg_image)
         self.bg_label.place(x=0,y=0)
 
-        self.logo_image = Image.open("assets/logo.jpg")
-        self.logo_image = ImageTk.PhotoImage(self.logo_image.resize((100, 100)))
-        self.logo_label = tk.Label(self, image=self.logo_image)
-        self.logo_label.place(x=250, y=150)
+        # self.logo_image = Image.open("assets/logo.jpg")
+        # self.logo_image = ImageTk.PhotoImage(self.logo_image.resize((100, 100)))
+        # self.logo_label = tk.Label(self, image=self.logo_image)
+        # self.logo_label.place(x=250, y=150)
 
-        self.heading_label = tk.Label(self, text="Save a life: Donate Blood!", font=("Didot", 25, "italic", "bold", "underline"), bg="white", fg="maroon", pady=15, borderwidth=4)
-        self.heading_label.place(x=400, y=170)
+    #     self.heading_label = tk.Label(self, text="Save a life: Donate Blood!", font=("Didot", 25, "italic", "bold", "underline"), bg="white", fg="maroon", pady=15, borderwidth=4)
+    #     self.heading_label.place(x=400, y=170)
 
-        self.about_label = tk.Label(self, text='''ABOUT US: Welcome to our Blood Bank Management System website!
-      We are dedicated to facilitating the vital process of blood donation and distribution
-      to save lives. Our platform offers an efficient and secure way for donors to register,
-      and track their donations. For hospitals and blood banks, our system provides
-      streamlined inventory management, real-time tracking of blood units, and
-      automated alerts for critical shortages.Together, we can make a significant 
-      impact in ensuring a steady supply of blood for those in need.''', font=("gadugi", 11), bg="white", fg="brown4", pady=15, borderwidth=4, relief=tk.RAISED)
-        self.about_label.place(x=250, y=330)
+    #     self.about_label = tk.Label(self, text='''ABOUT US: Welcome to our Blood Bank Management System website!
+    #   We are dedicated to facilitating the vital process of blood donation and distribution
+    #   to save lives. Our platform offers an efficient and secure way for donors to register,
+    #   and track their donations. For hospitals and blood banks, our system provides
+    #   streamlined inventory management, real-time tracking of blood units, and
+    #   automated alerts for critical shortages.Together, we can make a significant 
+    #   impact in ensuring a steady supply of blood for those in need.''', font=("gadugi", 11), bg="white", fg="brown4", pady=15, borderwidth=4, relief=tk.RAISED)
+    #     self.about_label.place(x=250, y=330)
 
-        self.donate_button = tk.Button(self, text="DONATE", font=("Lucida fax", 20, "bold"), relief=tk.RAISED, borderwidth=5,command=self.donor_page)
+        self.donate_button = tk.Button(self, text="DONATE", font=("Lucida fax", 15, "bold"), relief=tk.RAISED, borderwidth=5,command=self.donor_page)
         self.donate_button.config(bg="brown3")
-        self.donate_button.place(x=300, y=600)
+        self.donate_button.place(x=100, y=550)
         self.donate_button.bind('<Double-1>', self.quit)
 
-        self.need_button = tk.Button(self, text="NEED", font=("Lucida Fax", 20, "bold"), relief=tk.RAISED, borderwidth=5, command=self.acceptor_page)
+        self.need_button = tk.Button(self, text="NEED", font=("Lucida Fax", 15, "bold"), relief=tk.RAISED, borderwidth=5, command=self.acceptor_page)
         self.need_button.config(bg="brown3")
-        self.need_button.place(x=650, y=600)
+        self.need_button.place(x=320, y=550)
         self.need_button.bind('<Double-1>', self.quit)
 
     def acceptor_page(self):
